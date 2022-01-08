9203e0576c3006260113e1b4f9e224bfd95113fe
Ashish Ghimire <ashish.ghimire@docsumo.com>
2022-01-08 20:38:55 +0545

    v0.0.1: InstaClient: set parameters to make connection to instagram

--- smcon/structures/enums.py
+++ smcon/structures/enums.py
@@ -1,34 +1,75 @@
 from enum import Enum
 
-from smcon.clients.clients import InstaClient
+
 
 class ClientUrl(Enum):
     INSTA_URL = "https://www.instagram.com/"
     SPOTIFY_USL = "https://www.spotify.com/np-en/"
 
-class LoginUrl(Enum):
-    INSTA_URL = "https://www.instagram.com/"
-    SPOTIFY_USL = "https://www.spotify.com/np-en/"
+class LoginUrl:
+    INSTA_URL = "https://www.instagram.com/accounts/login/ajax/"
+    SPOTIFY_URL = "https://www.spotify.com/np-en/"
+
+
+    def __init__(self, instance) -> None:
+        self.instance = instance
+        self.get_url()
+    
+
+    def get_url(self):
+        """
+        """
+        # TODO
+        # Optimal method to do this
+        client = self.instance.CLIENT
+        if client in ["insta"]:
+            return LoginUrl.INSTA_URL
+        elif client in ["spotify"]:
+            return LoginUrl.SPOTIFY_URL
+        return ""
+
+    
+
 
 
 class LoginParams:
+    """
+    For each client you only have to add login params.
+    """    
     insta = {
             'username': '',
             'password': '',
             'queryParams': {},
             'optIntoOneTap': 'false'            
         }
-
-    def __init__(self, instance) -> None:
-        self.instance = instance
-        
-        ...
-        
-
-    Spotify_Params = {
+    
+    # soptify params
+    spotify = {
             'username': '',
             'password': '',
             'queryParams': {},
             'optIntoOneTap': 'false'            
         }
+
+
+    def __init__(self, instance) -> None:
+        self.instance = instance
+        self.get_params()
+    
+
+    def get_params(self):
+        """
+        """
+        # TODO
+        # Optimal method to do this
+        client = self.instance.CLIENT
+        if client in ["insta"]:
+            return LoginParams.insta
+        elif client in ["spotify"]:
+            return LoginParams.spotify
+        return {}
+
+
+        
+
 
